f487751bdd78c27058d92ee814b81b7be86ea265
n2god <n2god@o2.pl>
2019-02-03 18:31:28 +0100

    put new user added

--- src/main/java/RESTapiExample.java
+++ src/main/java/RESTapiExample.java
@@ -1,5 +1,6 @@
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -16,17 +17,10 @@ public class RESTapiExample {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        URI uri = new URIBuilder()
-                .setScheme("https")
-                .setHost("reqres.in")
-                .setPath("/api/users?page=2")
-                .build();
-        HttpGet httpget = new HttpGet(uri);
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        CloseableHttpResponse response = httpclient.execute(httpget);
-        String content = EntityUtils.toString(response.getEntity());
-        //content = content.substring(1, content.length() - 1); //opcjonalnie gdy content jest otoczony []
 
+
+
+        //content = content.substring(1, content.length() - 1); //opcjonalnie gdy content jest otoczony []
 
 
 
@@ -52,11 +46,33 @@ public class RESTapiExample {
         switch(Character.toUpperCase(letter))
         {
             case '1':
+                URI uri = new URIBuilder()
+                        .setScheme("https")
+                        .setHost("reqres.in")
+                        .setPath("/api/users?page=2")
+                        .build();
+
+                HttpGet httpget = new HttpGet(uri);
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+                CloseableHttpResponse response = httpclient.execute(httpget);
+                String content = EntityUtils.toString(response.getEntity());
+
                 JSONObject json = new JSONObject(content);
                 System.out.println(json);
                 break;
             case '2':
-                JSONObject newUser = new JSONObject(content);
+
+                URI uri2 = new URIBuilder()
+                        .setScheme("https")
+                        .setHost("reqres.in")
+                        .setPath("/api/users")
+                        .build();
+                HttpPut httpput = new HttpPut(uri2);
+                CloseableHttpClient httpclient2 = HttpClients.createDefault();
+                CloseableHttpResponse response2 = httpclient2.execute(httpput);
+                String content2 = EntityUtils.toString(response2.getEntity());
+
+                JSONObject newUser = new JSONObject(content2);
                 System.out.println(newUser);
                 break;
             case '3':
